e0f0bacc67ff858f419a95a098fd527a6895de88
Rebecca Tolmach <rmtolmach@gmail.com>
2016-03-29 12:31:31 -0700

    added conditional in new_or_edit method

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,8 +25,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.new
-    @category = Category.find(params[:id]) if params[:id]
+    params[:id] ? @category = Category.find(params[:id]) : @category = Category.new
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
